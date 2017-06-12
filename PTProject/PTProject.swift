d1457e62325ca2890e4789f11f219c1497441b5f
Byron Anderson <banderson@pivotal.io>
2017-06-12 16:01:54 -0600

    initial commit

---
+++ PTProject/PTProject.swift
@@ -0,0 +1,12 @@
+//
+//  PTProject.swift
+//  PTProject
+//
+//  Created by pivotal on 6/12/17.
+//  Copyright © 2017 pivotaltracker. All rights reserved.
+//
+
+func PTProjectIsCool() -> Bool {
+    return true;
+}
+
--- PTProjectTests/PTProjectTests.swift
+++ PTProjectTests/PTProjectTests.swift
@@ -22,15 +22,8 @@ class PTProjectTests: XCTestCase {
     }
     
     func testExample() {
+        XCTAssert(PTProject.PTProjectIsCool());
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
