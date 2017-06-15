1615882d2ba66d67eb7c4c9221e006d37a9fae49
Byron Anderson <banderson@pivotal.io>
2017-06-15 12:12:39 -0600

    mutant clones

--- PTProject/PTProject.swift
+++ PTProject/PTProject.swift
@@ -6,23 +6,40 @@
 //  Copyright © 2017 pivotaltracker. All rights reserved.
 //
 
+import Foundation
+
 struct PTProject : Equatable {
     let name : String;
     let iterationLength : Int;
     
     static func reduce(project: PTProject, action: ProjectAction) -> PTProject {
         return action.results.reduce(project, { (project, result) in
-            var newProject = project;
             if (result.type == "project") {
+                let details = serialize(project: project);
                 if (result.name != nil) {
-                    newProject = PTProject(name: result.name!, iterationLength: newProject.iterationLength);
+                    details.setValue(result.name!, forKey: "name")
                 }
                 if (result.iteration_length != nil) {
-                    newProject = PTProject(name: newProject.name, iterationLength: result.iteration_length!);
+                    details.setValue(result.iteration_length!, forKey: "iterationLength")
                 }
+                return deserialize(dictionary: details);
             }
-            return newProject;
+            return project;
         })
+    }
+    
+    static func serialize(project: PTProject) -> NSMutableDictionary {
+        return [
+            "name": project.name,
+            "iterationLength": project.iterationLength
+        ];
+    }
+    
+    static func deserialize(dictionary: NSDictionary) -> PTProject {
+        return PTProject(
+            name: dictionary.object(forKey: "name") as! String,
+            iterationLength: dictionary.object(forKey: "iterationLength") as! Int
+        )
     }
 }
 
